6ef6acf1970b2608e645787b92918b5bda6a9441
Ankitanand123 <117056428+Ankitanand123@users.noreply.github.com>
2024-06-24 12:38:32 +0530

    Create lecture19.cpp

---
+++ lecture19.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    long long int a;
+    cout<<"The size of long long int is = "<<sizeof(a)<<endl;
+    cout<<"The size of int is = "<<sizeof(int);
+    return 0;
+}
